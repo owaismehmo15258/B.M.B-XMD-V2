6adba3c363184f503662f9c8545b0ca66f64c521
owaismehmo15258 <tk8300089@gmail.com>
2025-05-18 10:49:22 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "BMB-XMD~PQADXKpB#13Dp5op-8S1-C8RStlLrUPS2LnV5VzCsqb9jrBXREsk",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -37,7 +37,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "255767862457",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "923112066705",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "ʙ.ᴍ.ʙ-xᴍᴅ-ᴠ2",
 // add bot owner name
